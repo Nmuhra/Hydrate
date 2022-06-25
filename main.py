299c30c30f1ac423e5ac135d61577726dc6eabfc
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-06-25 21:00:24 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+print("feeling dehydrated?, \nthis program will tell you how much water you need to drink based on your body weight")
+weight = float(input("what is your weight in Kg? "))
+print("you need", weight * 0.033, "liters of water")
